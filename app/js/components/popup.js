52a9494438c7a98dbcaf9a7ae1605b54cba759ce
Lexa <ily4.farmer@yandex.ru>
2021-07-26 18:46:37 +0300

    pay - изменена картинка, скрипт, поворот для стрелки и адаптив

--- app/js/components/popup.js
+++ app/js/components/popup.js
@@ -1,21 +1,18 @@
 
 const popup = () => {
-    const btn = document.querySelectorAll('.pay__footer-btn'),
-         list = document.querySelector('.pay__list');
+    const btn = document.querySelector('.pay__number'),
+          arrow = document.querySelector('.pay__footer-btn'),
+          list = document.querySelector('.pay__list');
     
-    btn[1].style.display = 'none';
-    list.style.display = 'none';
-    // 
-    btn[0].addEventListener('click', ()=> {
-        list.style.display = 'block';
-        btn[0].style.display = 'none';
-        btn[1].style.display = 'block';
-    });
 
-    btn[1].addEventListener('click', ()=> {
-        list.style.display = 'none';
-        btn[0].style.display = 'block';
-        btn[1].style.display = 'none';
+    btn.addEventListener('click', ()=> {
+        if (list.style.display == 'none') {
+            list.style.display = 'block';
+            arrow.classList.add("svg-top");
+        } else {
+            list.style.display = 'none';
+            arrow.classList.remove("svg-top");
+        }
     });
 }
 
